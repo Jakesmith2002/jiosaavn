48af3c00429938378d0e230879f36efbffb6d390
Jakesmith2002 <148589301+Jakesmith2002@users.noreply.github.com>
2024-12-01 17:03:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "LuffyZone"
+    return "JioBot"
 
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", "7676"))
+    port = int(os.environ.get("PORT", "7878"))
     app.run(host='0.0.0.0', port=port)
